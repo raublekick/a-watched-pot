07cf873e922b5f4e1aebff75e4f4bb9de217ce49
Andrew Raub <raublekick@gmail.com>
2022-01-15 23:25:18 -0700

    Pushign to gh-pages

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,14 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from "vite";
 import { createVuePlugin as vue } from "vite-plugin-vue2";
 const path = require("path");
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/a-watched-pot/",
   plugins: [vue()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-})
+});
